ee9a74c22ef1fab34ab1c5b7be6eced4ccc84623
moh4b-z <mohammadsalim10000@gmail.com>
2025-05-13 14:56:36 -0300

    ˜Adição˜

--- app/src/main/java/br/senai/sp/jandira/rick_mortyapi/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/rick_mortyapi/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import br.senai.sp.jandira.rick_mortyapi.screens.HomeScreen
 import br.senai.sp.jandira.rick_mortyapi.ui.theme.RickMortyAPITheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RickMortyAPITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
     }
